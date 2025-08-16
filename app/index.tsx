929b5908240c11182040e1bb0b23ba7a500bcf80
Amber Jain <jainanuamber@gmail.com>
2025-08-16 17:24:59 +0530

    convex setup completed ✅

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,17 @@
-import { Text, View } from "react-native";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { ActivityIndicator, Text, View } from "react-native";
 
 export default function App() {
+  const tasks = useQuery(api.tasks.getTasks);
+  if (tasks === undefined) {
+    return <ActivityIndicator />;
+  }
+  const task = tasks[0];
+  console.log(tasks);
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-xl font-bold text-blue-500">{task.text}</Text>
     </View>
   );
 }
